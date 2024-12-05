c1fbdb2f54c75a96229fec520392bd9dd7a61c3a
spaider <spaider.lu@gmail.com>
2024-12-05 21:31:16 +0530

    basisc of python

---
+++ basics/main.py
@@ -0,0 +1,42 @@
+# data types 
+# Int 
+-432484374475
+56485684564
+
+# Float 
+234.0
+-33454.6
+# String 
+"name"
+'name'
+
+# Boolean 
+True  
+False 
+
+# print("hello jyoti")
+# print("name")
+
+# variable 
+
+hello = "tim"
+world ="world"
+# print(hello, world)
+
+# user input 
+
+# name = input("Name: ")
+# age = input("Age: ")
+# print("Hello",name,"your are",age,"years old")
+
+
+# arithmetic operator
+# x = 9
+# y=10
+# result = x+y
+# print(result)
+
+# String methods
+
+hello ="hello"
+print(hello + "  jyoti")
