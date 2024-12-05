a5af93e7e71fed68641ed56ebd7cc804442de3bb
jyoti <paljyoti0129@gmail.com>
2024-12-05 22:53:04 +0530

    learnt basis like for while loop if else

--- basics/main.py
+++ basics/main.py
@@ -38,5 +38,97 @@ world ="world"
 
 # String methods
 
-hello ="hello"
-print(hello + "  jyoti")
+# hello ="hello"
+# print(hello + "  jyoti")
+
+
+
+# user input 
+# name = input("Enter number:")
+# print(name)
+
+
+# type()
+# num1 = 5
+# name = "jyoti"
+
+# print(type(num1))
+# print(type(name))
+
+# number = int(input("enter number"))
+# print(number)
+
+# multi line string 
+# if not asssigned  to any varible it will act like a comment
+# something = '''   this is a miltile
+
+# string
+# '''
+
+# print(something)
+
+
+# logical operator 
+
+# age = 19
+# gpa= 9
+# # result = age >= 18 and gpa > 10
+# # result = age >= 18 or gpa > 10
+# result = False
+# print( not result)
+
+
+
+# if else 
+# number =25
+# if number > 20 or number < 0:
+#     print("number is invalide")
+# elif number >= 15:
+#     print("passed")
+
+# else:
+#     print("failed")
+
+# while loop
+# number = 0
+# while number < 5:
+#     print(number)
+#     number = number + 1
+
+# print the table of 2
+# number = 1
+# while number <=10:
+#     print(2*number)
+#     number = number + 1
+
+
+
+# for loop 
+
+# name = "jyoti"
+# arr = ["jyoti","pal","29"]
+
+# for s in arr:
+#     print(s)
+
+
+# range in for loop
+
+# for count in range(1,10):
+#     print(count)
+
+
+# break and continue
+
+# for item in range(1,6):
+#     if item == 5:
+#         break
+#     print(item)
+# print("the end")
+
+
+# pass 
+number = 5.5
+if number > 0.0:
+    pass
+
